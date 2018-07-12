26f45fa77280d140ffa77d051871203ff3faedc4
jessicaxwang <jessicawang97@yahoo.com>
2018-07-12 18:05:59 -0400

    reinstalled package.json

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-var compress = require('compression');
-app.use(compress());
 
 app.use(express.static(__dirname + '/public'))
 
