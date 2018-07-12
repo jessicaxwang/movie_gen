428aa1dfb029b191c3bf7be674a32f659a14a748
jessicaxwang <jessicawang97@yahoo.com>
2018-07-12 17:59:35 -0400

    moved node modules

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 var express = require('express')
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+var compress = require('compression');
+app.use(compress());
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(process.env.PORT || 3000);
