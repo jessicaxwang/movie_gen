14a07e838379ba57f09f757ac957525f8c4738b0
jessicaxwang <jessicawang97@yahoo.com>
2018-07-12 18:44:39 -0400

    added onclick function

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(8888);
