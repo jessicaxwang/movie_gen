27b204bb180626a6ef99eb72430c33f530239895
jessicaxwang <jessicawang97@yahoo.com>
2018-07-12 21:57:21 -0400

    Added error message

--- public/gen.js
+++ public/gen.js
@@ -1,6 +1,5 @@
 window.onload = function(){
-    console.log("opened")
-    button_msg = ["Try again!", "Another!", "How about another, eh?", "Again!", "What to watch?", "So many choices!", "So...many...movies...", "OMG", "Yeet"];
+    button_msg = ["Try again!", "Another!", "How about another, eh?", "Again!", "What to watch?", "So many choices!", "So many movies!", "OMG", "Yeet", "One more time!", "Again! Again!"];
     document.getElementById("button").onclick = function () {
       var data = "https://movie-gen.herokuapp.com/movies.json";
       var request = new XMLHttpRequest();
@@ -10,33 +9,44 @@ window.onload = function(){
         request = request.responseText;
         parsed = JSON.parse(request);
 
-        if (parsed.hasOwnProperty('title')) {
-          movie = document.getElementById("movie");
-          movie.innerHTML = parsed['title'];
-        }
+        if (parsed.hasOwnProperty('title') && (parsed['title'] != " ")) {
+            movie = document.getElementById("movie");
+            movie.innerHTML = parsed['title'];
 
-        if (parsed.hasOwnProperty('original_language')) {
-        language = document.getElementById("language");
-        language.innerHTML = parsed['original_language'].toUpperCase();
-        }
+            if (parsed.hasOwnProperty('original_language') && (parsed['original_language'] != " ")) {
+              language = document.getElementById("language");
+              language.innerHTML = parsed['original_language'].toUpperCase();
+            } else {
+              language.innerHTML = " ";
+            }
 
-        if (parsed.hasOwnProperty('genres')) {
-          if (parsed['genres'].length > 0) {
-            genre = document.getElementById("genre");
-            genre.innerHTML = parsed['genres'][0]['name'];
-          }
-        }
+            if (parsed.hasOwnProperty('genres') && (parsed['genres'].length > 0)) {
+                genre = document.getElementById("genre");
+                genre.innerHTML = parsed['genres'][0]['name'];
+              } else {
+                genre.innerHTML = " ";
+              }
 
-        if (parsed.hasOwnProperty('overview')) {
-          overview = document.getElementById("overview")
-          overview.innerHTML = parsed['overview'];
-        }
+            if (parsed.hasOwnProperty('overview') && (parsed['overview'] != " ")) {
+                overview = document.getElementById("overview")
+                overview.innerHTML = parsed['overview'];
+              } else {
+                overview.innerHTML = " ";
+              }
+            } else {
 
-        button = document.getElementById("button");
-        idx = Math.floor(Math.random() * (button_msg.length));
-        button.innerHTML = button_msg[idx];
-      }
+              movie.innerHTML = "Click again!";
+              language.innerHTML = " ";
+              genre.innerHTML = " ";
+              overview.innerHTML = " ";
+            }
+        }
     }
+
+    button = document.getElementById("button");
+    idx = Math.floor(Math.random() * (button_msg.length));
+    button.innerHTML = button_msg[idx];
+
     request.send();
   }
 }
